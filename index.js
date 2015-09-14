cd4b2eca70d497d33d476a9d00e714bc86f06023
candlewoodtoby <johnsukoh@yahoo.com>
2015-09-13 21:38:59 -0700

    minor updates

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var app = express();
 var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 
 var mainController = require('./controllers/mainController.js');
 
+
+
 app.set('view engine','ejs');
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use('/', mainController);
+
 
 
 
