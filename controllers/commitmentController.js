c9c3297ce857c895a5722376749ebd9a3ce2dc50
candlewoodtoby <johnsukoh@yahoo.com>
2015-09-14 19:58:04 -0700

    added test CRUD for commitment controller

--- controllers/commitmentController.js
+++ controllers/commitmentController.js
@@ -1,34 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
+
+//FIND all Commitments
 router.get('/', function(req,res){
-  console.log("commitmentController.js working!");
-  res.send('commitmentController.js works!');
+
+
 })
 
+
+//FIND one Commitment
 router.get('/:commitmentId',function(req,res){
-  console.log('/commitment/:commitmentId route is working!');
-  res.send('/commitment/:commitmentId route is working!');
+
+
+});
+
+
+//UPDATE a Commitment
+router.put('/:commitmentId',function(req,res){
+
+
 })
 
-router.post('/',function(req,res){
-  // console.log('POST /commitment route is working!');
-  // console.log('req.body: ' + req.body);
-  // console.log('req.body.description: ' + req.body.description);
-  // console.log('req.body.trash: ' + req.body.trash);
-  // console.log('req.body.actionable :' + req.body.actionable );
-  // console.log('req.body.someday :' + req.body.someday );
-  // console.log('req.body.reference :' + req.body.reference );
-  // console.log('req.body.lessThanTwo :' + req.body.lessThanTwo );
-  // console.log('req.body.doItNow :' + req.body.doItNow );
-  // console.log('req.body.delegate :' + req.body.delegate );
-  // console.log('req.body.delegateName :' + req.body.delegateName );
-  // console.log('req.body.doItLater :' + req.body.doItLater );
-  // console.log('req.body.calendar :' + req.body.calendar );
-  // console.log('req.body.doItWhenever :' + req.body.doItWhenever );
-  // console.log('req.body.completed :' + req.body.completed );
-
-  res.send(req.body);
+//DELETE a user
+router.delete('/:commitmentId',function(req,res){
 
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
 var express = require('express');
-
 var router = express.Router();
 var db = require('../models');
 
+
+//FIND all Users
 router.get('/', function(req,res){
 
   db.user.findAll().then(function(users){
+
+    //TEST LOOP
     // if (users){
     // users.forEach(function(taco){
     // console.log('All users: ' + taco.id + '--'+ taco.username + '--' + taco.email + '--' + taco.password);
@@ -13,32 +16,40 @@ router.get('/', function(req,res){
     // })};
     // res.send('found all users!');
   })
+
+  //TEST
   // console.log("userController.js working!");
   // res.send('userController.js works!');
 
 })
 
+//FIND one User
 router.get('/:userId',function(req,res){
 
   db.user.find({where:{id:req.params.userId}}).then(function(user){
 
+    //TEST
     // console.log('req.params: ' + req.params);
     // console.log('req.params.userId' + req.params.userId);
     // console.log('found user!' +'--' + user.username + '--' + user.email + '--' + user.password)
     // res.send('You found the user!' + user.username);
   })
 
+    //TEST
     // console.log('/user/:userId route is working!');
     // res.send('/user/:userId route is working!');
 
 })
 
+//CREATE new User
 router.post('/', function(req,res){
   db.user.create({username:req.body.username,email:req.body.email,password:req.body.password})
   .then(function(user){
     // console.log('DB Entry worked! created new user' + user +'--'+ user.id +'--'+ user.username +'--'+ user.email +'--' + user.password);
     // res.send('DB entry worked! created new user:' +'--'+ user.id +'--'+ user.username +'--'+ user.email +'--' + user.password);
   })
+
+  //TEST
   // console.log('POST /user route is working!');
   // console.log(req.body);
   // console.log('req.body.username: ' + req.body.username);
@@ -50,23 +61,30 @@ router.post('/', function(req,res){
   // res.send('req.body.password: ' + req.body.password);
 });
 
+
+//UPDATE a User
 router.put('/:userId',function(req,res){
 
   db.user.find({where:{id:req.params.userId}}).then(function(user){
-    user.username = 'Taco';
+    user.username = 'Taco Jones';
     user.save().then(function(user){
       // console.log('update worked! username changed to:' + user.username)
     })
   })
 
+  //TEST
   // console.log('PUT /user/:userId  is working!!');
   // res.send('PUT /user/:userId  is working!!');
 })
 
+
+//DELETE a User
 router.delete('/:userId',function(req,res){
 
   db.user.find({where:{id:req.params.userId}}).then(function(user){
     user.destroy().then(function(){
+
+        //TEST
         // console.log('DELETE /user/:userId  worked!!');
         // res.send('DELETE /user/:userId  worked!!');
 
