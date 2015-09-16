24a7484a4683de4925f0398502699ad195fc2465
candlewoodtoby <johnsukoh@yahoo.com>
2015-09-15 19:49:12 -0700

    added CRUD operations for model action

--- controllers/actionController.js
+++ controllers/actionController.js
@@ -1,22 +1,87 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
+
+//FIND all Actions
 router.get('/', function(req,res){
-  console.log("actionController.js working!");
-  res.send('actionController.js works!!');
+  db.action.findAll({include:[db.commitment]}).then(function(action){
+    //TEST
+    // console.log(action);
+    if(action){
+      action.forEach(function(taco){
+        console.log(taco.id);
+        console.log(taco.description);
+        console.log(taco.completed)
+      })
+    } res.send('found actions!')
+
+  })
+  //TEST
+  // console.log("actionController.js working!");
+  // res.send('actionController.js works!!');
 })
 
+//FIND and Show one Action
 router.get('/:actionId',function(req,res){
-  console.log('/action/:actionId route is working!');
-  res.send('/action/:actionId route is working!');
+
+  db.action.find({where:{id:parseInt(req.params.actionId)}}).then(function(action){
+    //TEST
+    console.log('Action Id: ' + action.id + '--' + 'Description: '+ action.description + '--' + 'Completed?:'+ action.completed)
+    console.log('Action belongs to Commitment Id: ' + action.commitmentId);
+    res.send('found one action!')
+  })
+
 })
 
+
+//CREATE new Action
 router.post('/',function(req,res){
-  console.log('POST /action route is working!')
-  console.log(req.body);
-  console.log('req.body.description: ' + req.body.description);
-  console.log('req.body.completed: ' + req.body.completed);
-  res.send(req.body);
+
+  db.commitment.find( {where: { id:parseInt(req.body.commitmentId) } } ).then(function(commitment){
+
+    commitment.createAction({
+      description:  req.body.description,
+      completed:    req.body.completed
+    }).then(function(action){
+      console.log('Created new action!:');
+      console.log(action);
+      res.send('Created new action!')
+    })
+  })
+
+  // TEST
+  // console.log('POST /action route is working!')
+  // console.log(req.body);
+  // console.log('req.body.description: ' + req.body.description);
+  // console.log('req.body.completed: ' + req.body.completed);
+  // console.log('req.body.commitmentId: ' + req.body.commitmentId);
+  // res.send(req.body);
+})
+
+//UPDATE an Action
+router.put('/:actionId', function(req,res){
+  db.action.find({where:{id:parseInt(req.params.actionId)}}).then(function(action){
+    action.description = "I just updated this action! - WASH MY FEET";
+    action.save().then(function(action){
+      console.log('action update worked!');
+      res.send('action update worked!')
+    })
+  })
+})
+
+//DELETE an Action
+router.delete('/:actionId',function(req,res){
+  db.action.find({where:{id:parseInt(req.params.actionId)}}).then(function(action){
+
+    // console.log(action);
+    // console.log(action.id);
+    action.destroy().then(function(){
+      //TEST
+      console.log('Deleted Action!');
+      res.send('Deleted Action!');
+    })
+  })
 })
 
 module.exports = router;
